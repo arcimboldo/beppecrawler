c06876d6c8bc400e6c39cc37564a6681938cd9c9
Antonio Messina <antonio.s.messina@gmail.com>
2013-03-24 13:00:32 +0100

    Read settings from `settings.py` if found

--- web/manage.py
+++ web/manage.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 import os
+import site
 import sys
+
+try:
+    import settings
+    # load virtualenv if set
+    if settings.VIRTUALENV:
+        site.addsitedir(settings.VIRTUALENV)
+except:
+    # No `settings.py` file found.
+    pass
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web.settings")
